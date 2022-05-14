c67c9594c705302ed06e79baa6b51a581b2e61a5
Zakhar Likay <zakhar.likay@mail.ru>
2022-05-14 18:40:22 +0700

    Try to add db.relationships

--- app.py
+++ app.py
@@ -16,9 +16,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class dev_types(db.Model):
+
+class connector_types(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    num_of_pins = db.Column(db.Integer, nullable=False)
+    is_male = db.Column(db.Boolean, nullable=True)
+#    connectors = db.relationship('connectors', backref='connector_type', lazy=True)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class connectors(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    connector_type_id = db.Column(db.Integer, db.ForeignKey('connector_types.id'), nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
@@ -28,20 +39,25 @@ class container_types(db.Model):
     creation_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-class connector_types(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    num_of_pins = db.Column(db.Integer, nullable=False)
-    is_male = db.Column(db.Boolean, nullable=False)
-    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
-
-
 class containers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    cont_type_id = db.Column(db.Integer, db.ForeignKey('connector_types.id'), nullable=False)
+    cont_type_id = db.Column(db.Integer, db.ForeignKey('container_types.id'), nullable=False)
     container_id = db.Column(db.Integer, db.ForeignKey('containers.id'), nullable=True)
     creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class dev_types(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class pp_types(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 
 @app.route('/')
