54845048e94c257d609d7162dbd5eab330fc0679
Zakhar Likay <zakhar.likay@mail.ru>
2022-05-14 14:31:11 +0700

    Add some templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
@@ -19,6 +19,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    tables = make_table_list()
-    print(make_table_list())
+#    tables = make_table_list()
+#    print(make_table_list())
     app.run(debug=True)
